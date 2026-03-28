564180f27c8cf6b32a46e458fca1017694b7b0c8
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-03-28 12:32:32 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="Weather App">
-      <h1>Weather Application</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.instagram.com/kimberleythornton_/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Kimberley Thornton
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/kimberleyjthornton-debug/react-wearher-app-kjt"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open sourced
-        </a>{" "}
-        on GitHub
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <button ClassName="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.instagram.com/kimberleythornton_/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kimberley Thornton
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kimberleyjthornton-debug/react-wearher-app-kjt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourced
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
