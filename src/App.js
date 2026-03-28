524be2aeaf817af2235103ec9f872bea2687ee57
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-03-28 11:46:55 +0000

    Cleaned up App.js and added footer with GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <div className="Weather App"></div>;
+  return (
+    <div className="Weather App">
+      <h1>Weather Application</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.instagram.com/kimberleythornton_/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kimberley Thornton
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kimberleyjthornton-debug/react-wearher-app-kjt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open sourced
+        </a>{" "}
+        on GitHub
+      </footer>
+    </div>
+  );
 }
 
 export default App;
