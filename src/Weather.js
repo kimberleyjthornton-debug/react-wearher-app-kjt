0c48f6c22c9e0a4449464d08f84b2689b143df6b
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-03-29 11:33:46 +0100

    CSS styling applied

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
-export default function Weather(props) {
+export default function Weather() {
   let city = "Moscow";
   const [completed, executeCompleted] = useState(false);
   const [weatherData, setWeatherData] = useState({});
@@ -15,6 +15,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       humidity: response.data.temperature.humidity,
       description: response.data.condition.description,
+      pressure: response.data.temperature.pressure,
       iconUrl:
         "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png",
     });
@@ -35,7 +36,7 @@ export default function Weather(props) {
             <input
               type="button"
               value="Search"
-              className="search-form-button d-inline-block"
+              className="search-form-button"
             />
           </div>
         </form>
@@ -46,15 +47,25 @@ export default function Weather(props) {
           </li>
           <li>{weatherData.description}</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
-            <img src={weatherData.iconUrl} alt={weatherData.description} />
-            {Math.round(weatherData.temperature)}°C
+            <div className="d-flex">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                size={60}
+              />
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
           </div>
-          <div className="col-6">
+          <div className="col-6 mt-4">
             <ul>
               <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind} mph</li>
+              <li>Wind: {Math.round(weatherData.wind)} mph</li>
+              <li>Pressure: {Math.round(weatherData.pressure)} hPa</li>
             </ul>
           </div>
         </div>
