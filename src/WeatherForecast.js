cbecba449320e943f0f2bf48c9a1b2bb1b33d62f
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-03-30 12:47:44 +0100

    SheCodes forecast API integrated to WeatherForecast sheet. Props from Weather.js sent to render city from initial search

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./WeatherForecast.css";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+    console.log(props);
+  }
+
+  let apiKey = "o3d03f027be48ad05b5ec0c715cadt7d";
+  let city = "London";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast d-flex mt-4">
       <div className="row">
