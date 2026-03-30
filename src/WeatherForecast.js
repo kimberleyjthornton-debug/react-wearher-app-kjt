57e34ace40cbe95aa0e8f9101f8652f95540cca6
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-03-30 14:20:45 +0100

    useEffect integrated to allow refresh of daily forecast upon new user query entry

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
@@ -6,6 +6,10 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, forecastData] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.city]);
 
   function handleResponse(response) {
     console.log(response.data);
@@ -18,15 +22,19 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast d-flex mt-4">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.slice(0, 5).map(function (dailyForecast, index) {
+            return (
+              <div className="col ms-1" key={index}>
+                <WeatherForecastDay data={dailyForecast} />
+              </div>
+            );
+          })}
         </div>
       </div>
     );
   } else {
     let apiKey = "o3d03f027be48ad05b5ec0c715cadt7d";
-    let city = "London";
+    let city = props.city;
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
